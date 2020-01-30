6d1598138155127413b66cc305b1d63b547e8650
miguelt6 <miguel.t6@gmail.com>
2020-01-30 10:30:48 +0000

    Fix images to responsive

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,6 +4,7 @@ import "./card.styles.css";
 export const Card = props => (
   <div className="card-container">
     <img
+      className="monster-img"
       alt="monster"
       src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
     ></img>
