1cd2e4ca871b6b85c483c1ac28bdf98521733934
miguelt6 <miguel.t6@gmail.com>
2020-01-30 10:21:04 +0000

    Updates

---
+++ precache-manifest.665fa00a7ad63ceb5842827a15ed62e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4c422be3dc008f76baf1099979738a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e40eaec1cf2a21454a6b",
+    "url": "/monsters-rolodex/static/css/main.6e2b131b.chunk.css"
+  },
+  {
+    "revision": "b6b4b7653795c37734cb",
+    "url": "/monsters-rolodex/static/js/2.81bb5c18.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.81bb5c18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e40eaec1cf2a21454a6b",
+    "url": "/monsters-rolodex/static/js/main.6fe4ad24.chunk.js"
+  },
+  {
+    "revision": "420e781ce2f2f4d61faa",
+    "url": "/monsters-rolodex/static/js/runtime-main.9cf5dfda.js"
+  }
+]);
